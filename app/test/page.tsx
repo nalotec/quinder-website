25f9ca9e845e3f35f151bfcf07d94c39db9da060
nalotec <nalotec.business@gmail.com>
2025-12-11 14:24:36 -0600

    Actualizacion metadata
    Se actualiza el favicon del sitio con el logo de quinder
    Se actualiza la información del metadata
    Se agrega imagen de Open Graph

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
   return (
     <div>
-      <div>
-        <h1>Pagina de prueba</h1>
+      <div className="w-64 h-64 bg-sky-700 p-2 flex items-center justify-center">
+        <img src="/images/LogoSinFondo.png" alt="" className="h-full" />
       </div>
     </div>
   );
